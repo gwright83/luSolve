a63090691202c9c8761837c2960e64bfd76353ff
Gregory Wright <gwright@antiope.com>
2018-05-08 17:28:46 -0400

    Remove a commented-out line.

--- test/Numeric/LinearAlgebra/LUSolveSpec.hs
+++ test/Numeric/LinearAlgebra/LUSolveSpec.hs
@@ -34,7 +34,6 @@ randomColumnVectors n = Prelude.map (\vs -> M.fromLists (bundle 1 vs )) (bundle 
 
 checkLinearSystemSolution :: M.Matrix V.Vector Double
                           -> M.Matrix V.Vector Double -> Double
-                          -- -> (M.Matrix V.Vector Double, M.Matrix V.Vector Double)
 checkLinearSystemSolution a b = let
     x  = luSolve (luFactor a) b
     b' = matrixMultiply a x
